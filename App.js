c746f31bb058af7bd10bfcb53271f3590a0c19f8
PatilKavya <119836556+PatilKavya@users.noreply.github.com>
2023-01-11 11:32:44 +0530

    react

---
+++ App.js
@@ -0,0 +1,13 @@
+import ExpenceItem from './Components/ExpenceItem';
+
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenceItem></ExpenceItem>
+    </div>
+  );
+}
+
+export default App;
